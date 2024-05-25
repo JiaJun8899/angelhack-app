ee50612c9b7d8cb7f1fe2b34e92346e09e37b01a
JesS <90232280+waili990@users.noreply.github.com>
2024-05-26 02:16:34 +0800

    Login page

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
     let navigate = useNavigate(); 
     let location = useLocation();
     const routeChange = (newPath: string) => { 
-        navigate(newPath);
+        navigate(location.pathname + newPath);
       }
     
     return (
