4c6cf722e03a3b99fb55c019e4a244971d821ba3
DoubleBounce <cheekangchen@gmail.com>
2024-05-27 02:52:10 +0800

    hopefully this works

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite—plugin-pwa '
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Fingo',
+        short_name: 'Fingo',
+        description: 'A financial literacy app',
+        theme_color: '#000000',
+      }
+    })
   ],
 })
