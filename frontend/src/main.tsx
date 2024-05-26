1a32b12ba33c606f2c8ff4b8ff41c764acd5eab1
DoubleBounce <cheekangchen@gmail.com>
2024-05-27 01:54:30 +0800

    test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,21 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App';
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered: ', registration);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed: ', error);
-      });
-  });
-}
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+serviceWorkerRegistration.register();
